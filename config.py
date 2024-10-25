3017a932fd82cb58c2ede75797a039adefecc588
GREATPERSON <168116588+greatpersonxd@users.noreply.github.com>
2024-10-25 19:05:56 +0530

    Update config.py

--- config.py
+++ config.py
@@ -15,7 +15,7 @@ BOT_TOKEN = getenv("BOT_TOKEN")
 # -------------------------------------------------------
 OWNER_USERNAME = getenv("OWNER_USERNAME","legend_mickey")
 # --------------------------------------------------------
-BOT_USERNAME = getenv("BOT_USERNAME","doreamon_ro_bot")
+BOT_USERNAME = getenv("BOT_USERNAME","ZARA_MUSICBOT")
 # --------------------------------------------------------
 BOT_NAME = getenv("BOT_NAME")
 # ---------------------------------------------------------
@@ -51,8 +51,8 @@ GIT_TOKEN = getenv(
     "GIT_TOKEN", None
 )  # Fill this variable if your upstream repository is private
 
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/The_incricible")
-SUPPORT_CHAT = getenv("SUPPORT_CHAT","https://t.me/PARADISE_SOCIETY_OFFICIAL")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/PARADISE_SOCITEY")
+SUPPORT_CHAT = getenv("SUPPORT_CHAT","https://t.me/QUIZ_HUB_GROUP")
 
 # Set this to True if you want the assistant to automatically leave chats after an interval
 AUTO_LEAVING_ASSISTANT = bool(getenv("AUTO_LEAVING_ASSISTANT", False))
